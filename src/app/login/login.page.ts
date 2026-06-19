2c3d7e92ed826debe7ca3f4aafbee71859325a9f
joscar05-c <pizangojosec@gmail.com>
2026-06-19 17:38:28 -0500

    login cambiarmos de ruta

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { SupabaseService } from '../../services/supabase.service';
+import { SupabaseService } from '../services/supabase';
 
 @Component({
   selector: 'app-login',
